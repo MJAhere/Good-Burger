287ecddd3eaa8d1ddc71e276334165a68389d393
Michael Aiello <mjahere023@gmail.com>
2020-02-01 20:17:25 -0700

    Created connection.js.

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "19691969",
+        database: "burgers_db"
+    });
+}
+
+connection.connect(function (error){
+    if (error) {
+        console.error("error connecting: " + error.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
