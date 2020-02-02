5b956232cf1a4741013224531e73084b26be2d9e
Michael Aiello <mjahere023@gmail.com>
2020-02-01 22:51:35 -0700

    Update ReadMe.

--- Public/assets/js/burgers.js
+++ Public/assets/js/burgers.js
@@ -40,16 +40,16 @@ $(function() {
       );
     });
   
-    $(".delete-cat").on("click", function(event) {
+    $(".delete-burger").on("click", function(event) {
       var id = $(this).data("id");
   
-      
+     
       $.ajax("/api/burgers/" + id, {
         type: "DELETE"
       }).then(
         function() {
           console.log("deleted burger", id);
-        
+         
           location.reload();
         }
       );
