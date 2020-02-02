b37ad39ef93f3bccac2a27c51dcdc3d3ed59ac6b
Michael Aiello <mjahere023@gmail.com>
2020-02-01 22:17:44 -0700

    Worked out remaining files, including css and js ajax calls.

---
+++ Public/assets/js/burgers.js
@@ -0,0 +1,57 @@
+$(function() {
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newSleep = $(this).data("newsleep");
+  
+      var newSleepState = {
+        devoured: 1
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newSleepState
+      }).then(
+        function() {
+          console.log("changed sleep to", newSleep);
+          
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#burger").val().trim(),
+      };
+  
+      
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+         
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-cat").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+        
+          location.reload();
+        }
+      );
+    });
+  });
