1488d602a8ac197e5f95f3e3082005804c2c5abd
Michael Aiello <mjahere023@gmail.com>
2020-02-01 19:52:07 -0700

    Created schema and seed files.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Good Burge", false)
+, ("Extra Good Burge", false)
+, ("Crispy Kick Good Burger", false)
+, ("Triple Deluxe Cadillac Burger", false);
